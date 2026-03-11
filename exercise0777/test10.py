b869893e1668c2058cf513cc2a027c41307bee82
Boyan419329 <1527794550@qq.com>
2026-03-11 16:49:32 +0800

    Add files via upload

---
+++ exercise0777/test10.py
@@ -0,0 +1,23 @@
+# 输出十次HelloWorld
+
+i = 0
+while i<10:
+    print("HelloWorld")
+    i = i + 1
+
+# 输出1-100的所有的值
+
+i=1
+while i <= 100:
+    print(i)
+    i = i + 1
+
+
+# 计算1-100的和
+
+sum = 0 # 用来累计和
+i=1
+while i <= 100:
+    sum = sum + i
+    i = i + 1
+print (sum)
