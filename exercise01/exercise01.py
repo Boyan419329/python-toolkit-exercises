33ddadcdadbd768ada541732f5345b00bc2c9980
Boyan419329 <1527794550@qq.com>
2026-03-12 14:00:02 +0800

    Add files via upload

---
+++ exercise01/exercise01.py
@@ -0,0 +1,15 @@
+"""
+1. Python3.5及以上版本的保留总字数是
+A. 35  B.27  C.16  D.29
+
+2. 以下选项中，不是Python语言保留字的是
+A. while  B.except  C.do   D.pass
+
+3.下列选项中不符合Python语言变量命名规则的是
+A. TempStr  B.I  C.3_1  D._AI
+
+4.以下选项中，符合Python语言变量命名规则的是
+A. Templist B.！I  C.5_1  D.(VR)
+
+
+"""
---
+++ exercise01/test1.py
@@ -0,0 +1,12 @@
+print("HelloWorld") #rint()函数用来输出内容，也就是Print()中有什么，就输出什么
+print("HelloKitty")
+print("您好，世界")
+
+"""
+  以上是今天的第一个程序
+  这个程序的名字叫做HelloWorld
+"""
+
+'''
+
+'''
